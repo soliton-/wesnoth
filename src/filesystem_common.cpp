6ea9067c9509e4471634b2eff7bb9f624169b292
Gunter Labes <soliton@wesnoth.org>
2025-09-26 14:14:48 +0200

    Remove confusing locale dir differences

--- src/filesystem_common.cpp
+++ src/filesystem_common.cpp
@@ -236,21 +236,14 @@ std::string get_core_images_dir()
 
 std::string get_intl_dir()
 {
-#ifdef _WIN32
-	return game_config::path + "/" LOCALEDIR;
-#else
-
 #ifdef USE_INTERNAL_DATA
 	return get_cwd() + "/" LOCALEDIR;
 #endif
 
 #if HAS_RELATIVE_LOCALEDIR
-	std::string res = game_config::path + "/" LOCALEDIR;
+	return game_config::path + "/" LOCALEDIR;
 #else
-	std::string res = LOCALEDIR;
-#endif
-
-	return res;
+	return LOCALEDIR;
 #endif
 }
 
